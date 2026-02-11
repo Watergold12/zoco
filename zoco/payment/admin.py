d763e8606681c03d7f6792b8cb61811e1323a97d
Watergold12 <techieman7@gmail.com>
2026-02-11 17:44:55 +0530

    Worked on Checkout Logic

--- zoco/payment/admin.py
+++ zoco/payment/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Shipping_Address
+from .models import Shipping_Address, Order, OrderItem
 
 # Register your models here.
 admin.site.register(Shipping_Address)
+admin.site.register(Order)
+admin.site.register(OrderItem)
