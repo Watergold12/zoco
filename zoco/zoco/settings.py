901aea5660a11bb0f883b32a2809a9b5e8b6f250
WaterGold12 <techieman7@gmail.com>
2026-06-05 22:30:45 +0530

    Setup Railway PostgreSQL

--- zoco/zoco/settings.py
+++ zoco/zoco/settings.py
@@ -11,22 +11,27 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR /'.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&gu20_ztobji^$xlvfzs&k!%v6h92zz#p1kiimrhsq1p=3jsd3'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
@@ -82,11 +87,11 @@ WSGI_APPLICATION = 'zoco.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'pchClTqKIhPefxCbCVDcJUsiOUppktcg',
-        'HOST': 'acela.proxy.rlwy.net',
-        'PORT': '56451',
+        'NAME': os.getenv('PGDATABASE'),
+        'USER': os.getenv('PGUSER'),
+        'PASSWORD': os.getenv('PGPASSWORD'),
+        'HOST': os.getenv('PGHOST'),
+        'PORT': os.getenv('PGPORT'),
         'OPTIONS': {
             'sslmode': 'require',
         },
